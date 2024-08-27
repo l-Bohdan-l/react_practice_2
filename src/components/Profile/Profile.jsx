93f20430023dae4f5dbee16dac4aeb432348e7f6
Bohdan <bpapushoj@gmail.com>
2024-08-27 11:43:36 +0300

    add some styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,10 +5,12 @@ export default function Profile({ name, tag, location, image, stats }) {
   return (
     <div className={css.wrapper}>
       <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p> <p>@{tag}</p> <p>{location}</p>
+        <img className={css.image} src={image} alt="User avatar" />
+        <p className={css.userName}>{name}</p>
+        <p className={css.userInfo}>@{tag}</p>
+        <p className={css.userInfo}>{location}</p>
       </div>
-      <ul>
+      <ul className={css.userStatsList}>
         <li>
           <span>Followers</span> <span>{followers}</span>
         </li>
