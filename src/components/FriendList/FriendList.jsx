da4024d56e54a7d86943d6817b8d088886f9a83e
Bohdan <bpapushoj@gmail.com>
2024-08-30 10:35:07 +0300

    finish friend styles

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,8 +1,9 @@
+import css from "./FriendList.module.css";
 import FriendListItem from "./FriendListItem/FriendListItem";
 
 export default function FriendList({ friends }) {
   return (
-    <ul>
+    <ul className={css.friendList}>
       {friends.map((friend) => {
         return (
           <li key={friend.id}>
--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,10 +1,18 @@
+import clsx from "clsx";
+
+import css from "./FriendListItem.module.css";
+
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <div>
-      <img src={avatar} alt="user avatar" />
-      <p>{name}</p>
-      {isOnline && <p>Online</p>}
-      {!isOnline && <p>Offline</p>}
+    <div className={css.friendListItem}>
+      <img className={css.friendLogo} src={avatar} alt="user avatar" />
+      <p className={css.friendName}>{name}</p>
+      {isOnline && (
+        <p className={clsx(css.friendStatus, css.isOnline)}>Online</p>
+      )}
+      {!isOnline && (
+        <p className={clsx(css.friendStatus, css.isOffline)}>Offline</p>
+      )}
     </div>
   );
 }
