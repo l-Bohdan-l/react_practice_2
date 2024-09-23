bacf9bc66814c8784b01bc5fe6c67d7f2673690d
Bohdan <bpapushoj@gmail.com>
2024-09-23 22:24:42 +0300

    upd fetchImg func

--- src/services/fetchImages.js
+++ src/services/fetchImages.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 axios.defaults.baseURL = "https://pixabay.com/api/";
-const API_KEY = "24097500-b1b25815474c0bcb76303e859";
-
+const API_KEY = import.meta.env.VITE__PHOTO_API_KEY;
 export default async function fetchImages(searchQuery, page) {
   const url = `?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
   const response = await axios.get(url);
