cf21d1ae673431eb466bd8f9e9363de20fb3a3e5
Bohdan <bpapushoj@gmail.com>
2024-12-25 11:32:29 +0200

    add lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useEffect, useState } from "react";
 // import { BallTriangle } from "react-loader-spinner";
+import { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
@@ -7,8 +8,10 @@ import SharedLayout from "./components/SharedLayout/SharedLayout";
 import HomePage from "./pages/HomePage/HomePage";
 import Movies from "./pages/Movies/Movies";
 import MovieDetails from "./pages/MovieDetails/MovieDetails";
-import Cast from "./components/Movies/Cast/Cast";
-import Reviews from "./components/Movies/Reviews/Reviews";
+// import Cast from "./components/Movies/Cast/Cast";
+// import Reviews from "./components/Movies/Reviews/Reviews";
+const Cast = lazy(() => import("./components/Movies/Cast/Cast"));
+const Reviews = lazy(() => import("./components/Movies/Reviews/Reviews"));
 
 function App() {
   const router = createBrowserRouter([
@@ -31,11 +34,20 @@ function App() {
           children: [
             {
               path: "cast",
-              element: <Cast />,
+              element: (
+                <Suspense fallback={<div>Loading cast...</div>}>
+                  <Cast />
+                </Suspense>
+              ),
             },
             {
               path: "reviews",
-              element: <Reviews />,
+
+              element: (
+                <Suspense fallback={<div>Loading ...</div>}>
+                  <Reviews />
+                </Suspense>
+              ),
             },
           ],
         },
